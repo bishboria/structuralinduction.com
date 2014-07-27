6973bf494c33d5a649557c4abfacfc1b978eff17
Stuart Gale <bishboria@gmail.com>
2014-07-27 17:35:39 +0100

    Add proper routes. Add basic index and stylesheet

--- Main.hs
+++ Main.hs
@@ -5,7 +5,13 @@ import Web.Scotty
 
 main :: IO ()
 main = do
-    port <- getEnv "PORT"
-    scotty (read port) $
-      get "/" $
-        text "Hello, world!"
+    port  <- getEnv "PORT"
+
+    scotty (read port) $ do
+        get "/" $ do
+            file "index.html"
+
+        get "/style.css" $ do
+            file "style.css"
+
+        notFound $ file "404.html"
