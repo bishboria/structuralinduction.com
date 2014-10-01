6f5e819bf5f862e3d0b7618055bc4ecd4f764b80
Stuart Gale <bishboria@gmail.com>
2014-10-01 16:54:38 +0100

    Add sabbatical post

--- Main.hs
+++ Main.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad.IO.Class
+import Data.Monoid (mconcat)
+import Data.Text
+import System.Directory
 import System.Environment
+import System.FilePath
 import Web.Scotty
 
 main :: IO ()
@@ -9,9 +14,21 @@ main = do
 
     scotty (read port) $ do
         get "/" $ do
-            file "index.html"
+            file "static/index.html"
 
         get "/style.css" $ do
-            file "style.css"
+            file "static/style.css"
 
-        notFound $ file "404.html"
+        get "/blog/:post" $ do
+            p     <- param "post"
+            files <- allPosts
+            if   p `elem` files
+            then file ("static/posts/" ++ p ++ ".html")
+            else file "static/404.html"
+
+        notFound $ file "static/404.html"
+
+allPosts = do
+    files <- liftIO $ getDirectoryContents "static/posts"
+    let fs = Prelude.map takeBaseName files
+    return fs
